4b2f01dab89d5597022f0a8d5d5b587d593c847d
swFang <33407178+swFang@users.noreply.github.com>
2019-02-03 10:22:46 -0800

    finished factoring out prop components

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,22 @@
 import React from "react";
-import faker from "faker";
+//import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = props => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.dp} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          Julia
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date"> Today at 6:00 PM </span>
+          <span className="date"> {props.timeAgo} </span>
         </div>
-        <div className="text">Nice blogpost!</div>
+        <div className="text">
+          {props.content}
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail'
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import faker from 'faker'; 
 
 const App = () => {
-    return (
-        <div className = "ui container comments">
-            <CommentDetail/>
-        </div>
-    );  
+  return (
+    <div className="ui container comments">
+      <CommentDetail author="Sam" content="test comment!" timeAgo="Just Now" dp={faker.image.avatar()} />
+      <CommentDetail author="Tom" content="Hey! hows it going?" timeAgo="Today at 7:00pm"  dp={faker.image.avatar()}/>
+      <CommentDetail author="Jane" content="REEEEEE!" timeAgo="Today at 6:00pm" dp={faker.image.avatar()}/>
+      <CommentDetail author="Janet" content="jajajajajaja!" timeAgo = "Today at 5:00pm"  dp={faker.image.avatar()}/>
+    </div>
+  );
 };
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
